2c89640517a8b133106a5368bf2ee76ba94fa832
Ken <rg4093@naver.com>
2022-11-13 20:59:39 +0900

    edit pdf save test

--- src/components/PdfForm.js
+++ src/components/PdfForm.js
@@ -14,7 +14,7 @@ const PdfForm = () => {
         <button onClick={() => pdfSave("view")}>pdf 보기</button>
       </div>
       <div className="div_paper">
-        <h3>작업일보</h3>
+        <h1>작업일보</h1>
         <table>
           <tbody>
             <tr>
@@ -38,13 +38,21 @@ const PdfForm = () => {
             <tr>
               <th>금일 작업내용</th>
               <td colSpan={6}>
-                testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
+                금속류 구조체를 사용하여 도로, 교량, 터널 및 기타의 장소에 안전,
+                경계, 방호 시설물을 설치하거나 각종 구조물 및 공작물을
+                축조하거나 설치합니다. 창호공사, 벽체 및 칸막이 공사, 가드 레일,
+                표지판, 방호울타리, 휀스, 방음벽, 버스승강대 및 도로교통안전
+                시설물 등을 공사합니다.
               </td>
             </tr>
             <tr>
               <th>명일 작업내용</th>
               <td colSpan={6}>
-                testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
+                금속류 구조체를 사용하여 도로, 교량, 터널 및 기타의 장소에 안전,
+                경계, 방호 시설물을 설치하거나 각종 구조물 및 공작물을
+                축조하거나 설치합니다. 창호공사, 벽체 및 칸막이 공사, 가드 레일,
+                표지판, 방호울타리, 휀스, 방음벽, 버스승강대 및 도로교통안전
+                시설물 등을 공사합니다.
               </td>
             </tr>
 
@@ -56,17 +64,21 @@ const PdfForm = () => {
 
             <tr>
               <td colSpan={3}>
-                testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
+                홍길동 소장 남 대한금속
+                <br /> 정우성 과장 남 대한금속
+                <br /> 김희선 사원 여 대한금속
               </td>
               <td colSpan={3}>
-                testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
+                장현석 남 대한금속
+                <br /> 이정재 남 대한금속
+                <br /> 박재민 남 대한금속
               </td>
             </tr>
 
             <tr>
               <th>특이사항</th>
               <td colSpan={6}>
-                testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
+                금속류 구조체를 사용하여 도로, 교량, 터널 및 기타의 장소에 설치
               </td>
             </tr>
           </tbody>
--- src/lib/make_pdf.js
+++ src/lib/make_pdf.js
@@ -3,15 +3,15 @@ import jsPDF from "jspdf";
 
 const makePdf = async (state) => {
   const input = document.querySelector(".div_container > .div_paper");
-  await html2canvas(input, { logging: true, scale: 4 }).then((canvas) => {
+  await html2canvas(input, { logging: true, scale: 2 }).then((canvas) => {
     const imgData = canvas.toDataURL("image/png");
 
     const imgWidth = 210; // 이미지 가로 길이(mm) A4 기준
-    const pageHeight = imgWidth * 1.4; // 출력할 한 페이지 세로 길이 계산 A4 기준
+    const pageHeight = imgWidth * 1.424; // 출력할 한 페이지 세로 길이 계산 A4 기준
     const imgHeight = (canvas.height * imgWidth) / canvas.width; //전체 canvas 길이
     let heightAll = imgHeight;
 
-    const pdf = new jsPDF("p", "mm");
+    const pdf = new jsPDF("p", "mm", "a4");
     let position = 0;
 
     // 첫 페이지 출력
@@ -23,6 +23,7 @@ const makePdf = async (state) => {
       position = heightAll - imgHeight;
       pdf.addPage();
       pdf.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
+
       heightAll -= pageHeight;
     }
 
